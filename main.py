1789b15d3fe6b20a2d8c2683afe2c9b55c78fc48
LuizBevilacqua <luize.bevilacqua@gmail.com>
2024-03-07 12:46:20 -0300

    pressPress Pronto

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import numpy as np
 from solucao import solucaoPressPress
+import matplotlib.pyplot as plt
 
-po = 1900000
-pw = 90000
+po  = 19000000
+pw  = 9000000
+qw  = 0.01
+mi  = 0.001
+k   = 9.869233e-14
+h   = 10
 phi = 0.2
-mi = 0.5
-k = 500
-L = 100
-ct = 10e-6
-x = np.linspace(0, L, 1000)
+ct  = 2.04e-9
+L   = 10
+A   = 30
+x = np.linspace(1, L, 1000)
 t = np.linspace(0, 100, 10)
 
 solucao = solucaoPressPress(po,pw,phi,mi,k,L,ct)
-p= np.zeros((len(x),1))
+p = np.zeros((len(t),len(x)))
 
-p[:][0] = solucao.PressPress(x,t[5])
+for i in range(len(t)):
+    p[i][:] = solucao.PressPress(x,t[i])
+
+fig , ax = plt.subplots()
+plt.ylabel('Press (psi)')
+plt.xlabel('Comprimento (m)')
+plt.title('PRESSÃO PRESSÃO - ANALÍTICO')
+
+for i in range(len(t)):
+    ax.plot(x,p[i][:], label='Press Press Analítica')
+
+plt.show()
--- solucao.py
+++ solucao.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 
 class solucaoPressPress:
     def __init__(self,po,pw,phi,mi,k,L,ct):
@@ -14,17 +13,16 @@ class solucaoPressPress:
     def PressPress(self,x,t):
         p = np.zeros(len(x))
         sumOld = 100
-        err = 100
-        eppara = 10^-2
-        n = 1
+        err = 1000
+        eppara = 1e-2
+        n = 0
         sum = 0
-        #while err>eppara:
-         #   sum += np.exp(-(n*np.pi/self.L)**2*(self.k/(self.phi*self.mi*self.ct))*t)*np.sin(n*np.pi*x/self.L)
-          #  n = n + 1
-           # err = abs((sum-sumOld)/sum)*100
-           # sumOld = sum 
-        #p = (self.po - self.pw)*(x/self.L+2/np.pi*sum)+self.pw
-        for j in range(1,10000):
-            sum += (np.exp(-((j*np.pi/self.l)**2)*(self.k/(self.phi*self.mi*self.ct))*t)/j*np.sin(j*np.pi*x/self.l))
+        while err >= eppara:
+            n += 1
+            i = n-1
+            sum += (np.exp(-((n*np.pi/self.l)**2)*(self.k/(self.phi*self.mi*self.ct))*t)/n*np.sin(n*np.pi*x/self.l))
+            p = (self.po - self.pw)*((x/self.l)+(2/np.pi)*sum)+self.pw
+            err = abs((sum[i]-sumOld)/sum[i])*100
+            sumOld = sum[i]
         p = (self.po - self.pw)*((x/self.l)+(2/np.pi)*sum)+self.pw
         return p
